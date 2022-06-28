3015f291108ab853f572d5efd18b898c8a30c49f
Hermogenes Ferreira <hermogenes.ferreira@gmail.com>
2022-06-28 14:15:10 -0300

    correção 4

--- 4.js
+++ 4.js
@@ -3,6 +3,4 @@ var caminhao = 80; //caminhao saiu do ponto 100 com velocidade de 80km/h.
 var tempo = 100 / (carro + caminhao) + 0.1; //dividindo distância, pelas velocidades dos veículos, e adicionando os 10 minutos que o caminhão gasta nos pedágios.
 
 var resultado = carro * tempo; // o resultado é a velocidade do carro dividido pelo tempo;
-console.log(resultado);
-
-//o
+console.log(`Os veiculos vão se encontrar em ${resultado} km de distância.`);
